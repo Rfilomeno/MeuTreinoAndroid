5c3fa1408a17c8a5730ca7fa81426a358dcc8052
Rodrigo Filomeno <rodrigofilomeno@Mac-mini-de-Rodrigo.local>
2018-01-05 16:15:44 -0200

    Fim do projeto

--- app/src/main/java/br/com/filomeno/meutreino/ListaExerciciosActivity.java
+++ app/src/main/java/br/com/filomeno/meutreino/ListaExerciciosActivity.java
@@ -4,8 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
 
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -13,6 +17,7 @@ public class ListaExerciciosActivity extends AppCompatActivity {
 
     public ArrayList<Exercicio> ListaExercicios;
     ListView lv;
+    ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,7 @@ public class ListaExerciciosActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lista_exercicios);
 
         lv = (ListView) findViewById(R.id.exerciciosLV);
+        iv = (ImageView) findViewById(R.id.ivExecicio);
 
         ListaExercicios = new ArrayList<>();
         Intent it = getIntent();
@@ -33,6 +39,16 @@ public class ListaExerciciosActivity extends AppCompatActivity {
         );
 
         lv.setAdapter(adapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Exercicio exercicio = (Exercicio) parent.getItemAtPosition(position);
+
+                Picasso.with(getApplicationContext()).load(exercicio.Img).into(iv);
+
+            }
+        });
 
 
 
